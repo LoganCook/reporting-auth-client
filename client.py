130fe9ce2aec1ec1e53ad2e7e518db08e070462e
Jianfeng Li <jianfeng.li@ersa.edu.au>
2018-02-02 12:36:31 +1030

    Finished functions

--- client.py
+++ client.py
@@ -20,8 +20,8 @@ karl.key
 ent_karl = client.get(karl.key)
 
 
-query = client.query(kind='Account', projection=('email', 'timestamp'))
-query.add_filter('name', '=', 'Karl Sellmann')
+account_query = client.query(kind='Account', projection=('email', 'timestamp'))
+account_query.add_filter('name', '=', 'Karl Sellmann')
 
 def get_keyof(client, kind, key_filter):
     query = client.query(kind=kind)
@@ -29,11 +29,11 @@ def get_keyof(client, kind, key_filter):
     query.keys_only()
     return list(query.fetch())[0]
 
-def get_endpoint(client, endpoint):
+def get_endpoint_key(client, endpoint):
     """Get the key of an Endpoint by its name"""
     return get_keyof(client, 'Endpoint', ('name', '=', endpoint))
 
-def get_account(client, email):
+def get_account_key(client, email):
     """Get the key of an Account by its name"""
     return get_keyof(client, 'Account', ('email', '=', email))
 
@@ -46,3 +46,33 @@ def grant_access(client, account, endpoint):
     authorisation['timestamp'] = datetime.datetime.utcnow()
     client.put(authorisation)
     return authorisation.key
+
+def list_accesses(client):
+    query = client.query(kind='Account')
+    for account in query.fetch():
+        get_accessess(client, account)
+
+def get_accessess(client, account):
+    query = client.query(kind='Authorisation')
+    query.add_filter('account', '=', account.key)
+    print("%s has access to:" % account['name'])
+    for access in query.fetch():
+        endpoint = client.get(access['endpoint'])
+        print(endpoint['name'])
+
+def verify_access(client, account, endpoint):
+    """Verify if an Account has access of an Endpoint"""
+    # both need to be key
+    query = client.query(kind='Authorisation')
+    query.add_filter('account', '=', account.key)
+    query.add_filter('endpoint', '=', endpoint.key)
+    return len(list(query.fetch())) > 0
+
+def batch_grant(client, endpoint_name):
+    """Grant access to an Endpoint to all accounts if they don't have yet"""
+    endpoint = get_endpoint_key(client, endpoint_name)
+    query = client.query(kind='Account')
+    for account in query.fetch():
+        if not verify_access(client, account, endpoint):
+            print("Granting access to %s on %s" % (account['name'], endpoint))
+            grant_access(client, account, endpoint)
